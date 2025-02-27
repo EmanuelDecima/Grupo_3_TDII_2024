79e7f182b89b3be12b2bb32ed7c9f10aeff1d4e9
EmanuelDecima <emanueldecima3@gmail.com>
2025-02-27 16:17:33 -0300

    Agrego Comentarios en Secuencia_Inicio

--- AFP_5_TDII_2024 _STM32F401/Drivers/API/Src/RADAR_SECUENCIAS.c
+++ AFP_5_TDII_2024 _STM32F401/Drivers/API/Src/RADAR_SECUENCIAS.c
@@ -7,11 +7,17 @@
 #include "main.h"
 #include "RADAR_SECUENCIAS.h"
 
+/*
+ * @brief Secuencia al Encender el dispositivo
+ * @param None
+ * @retval None
+ */
 void Secuencia_Inicio(){
 	//Salidas Digitales
 	HAL_GPIO_WritePin(LED_ALARM_GPIO_Port, LED_ALARM_Pin, GPIO_PIN_RESET);
 	HAL_GPIO_WritePin(LED_ON_GPIO_Port, LED_ON_Pin, GPIO_PIN_SET);
 	HAL_GPIO_WritePin(LED0_GPIO_Port, LED0_Pin, GPIO_PIN_RESET);
+	HAL_GPIO_WritePin(BUZZER_GPIO_Port, BUZZER_Pin, GPIO_PIN_RESET);
 
 	//Mensaje por LCD
 	LCD_Clear();
@@ -19,9 +25,9 @@ void Secuencia_Inicio(){
 	LCD_SendString("SISTEMA");
 	LCD_PutCur(1, 0);
 	LCD_SendString("ACTIVADO");
+
 	//Mensaje por Bluetooth
 	HC05_SendString("SISTEMA ACTIVADO\n");
-	//HAL_GPIO_WritePin(LED0_GPIO_Port, LED0_Pin, GPIO_PIN_RESET);
 	HAL_Delay(3000);
 	LCD_Clear();
 }
